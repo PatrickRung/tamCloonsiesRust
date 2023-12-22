2b8fe6ca38a311e6265e86f5ce0acac70c99f8bf
PatrickR <Patrick.rung23@gmail.com>
2023-12-21 21:07:49 -0800

    npc

---
+++ Assets/Scripts/Bots/NPCScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : Button
+{
+    GameObject WorldStorage, ItemDesc;
+    private void Awake()
+    {
+        
+    }
+    public override void buttonInteraction()
+    {
+        
+    }
+}
