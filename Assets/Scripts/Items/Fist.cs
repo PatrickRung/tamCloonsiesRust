c9336a10ef91839904ed174eb4db2b35d7236d69
Jaden Ermish <129796249+Jermish0@users.noreply.github.com>
2024-02-08 21:20:24 -0800

    half done melee mechanic

--- Assets/Scripts/Items/Fist.cs
+++ Assets/Scripts/Items/Fist.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class Fist : MonoBehaviour
 {
     public float hitRange;
+    public bool readyToHit;
+    public float punchCooldown = 5.0f;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        Debug.Log(readyToHit);
+        if (Input.GetMouseButtonDown(0) && readyToHit)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.parent.position, transform.TransformDirection(Vector3.forward), out hit, hitRange))
+            punch();
+        }
+    }
+
+    void punch()
+    {
+        
+        readyToHit = false;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.parent.position, transform.TransformDirection(Vector3.forward), out hit, hitRange))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            Debug.Log(hit.transform.gameObject.GetComponent<EnemyAi>());
+            if (!object.ReferenceEquals(hit.transform.gameObject.GetComponent<EnemyAi>(), null))
             {
-                Debug.Log(hit.transform.gameObject.name);
-                Debug.Log(hit.transform.gameObject.GetComponent<EnemyAi>());
-                if (!object.ReferenceEquals(hit.transform.gameObject.GetComponent<EnemyAi>(), null))
-                {
-                    Debug.Log("punch thrown");
-                    hit.transform.gameObject.GetComponent<EnemyAi>().changeHealth(-20);
-                }
+                Debug.Log("punch thrown");
+                Vector3 direction = hit.transform.position - transform.position;
+                GameObject.Find("pill").GetComponent<Rigidbody>().AddForce(1000 * (direction.normalized));
+                hit.transform.gameObject.GetComponent<EnemyAi>().changeHealth(-20);
+                GameObject.Find("pill").GetComponent<Rigidbody>().AddForce(1000 * (-direction.normalized));
+                hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(1000 * (direction.normalized));
             }
         }
+        Invoke("ResetPunch", 1.0f);
+    }
+
+    void ResetPunch()
+    {
+        readyToHit = true;
     }
 }
